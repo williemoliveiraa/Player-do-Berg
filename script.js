633a72642f9287249fc65bd52b8f8d36287727e4
httpwilliem <86989569+httpwilliem@users.noreply.github.com>
2023-03-06 13:39:39 -0300

    Commit 3
    Inclusão do JavaScript no projeto.

---
+++ script.js
@@ -0,0 +1,11 @@
+const songName = document.getElementById('song-name');
+const song = document.getElementById('audio');
+const play = document.getElementById('play');
+
+songName.innerText = 'Rising';
+
+function playSong(){
+    song.play()
+}
+
+play.addEventListener('click', playSong);
