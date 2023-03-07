3fa09e7f758ed173c515ea03674e23a7b7363b31
httpwilliem <86989569+httpwilliem@users.noreply.github.com>
2023-03-07 01:15:18 -0300

    Commit 5
    Inclusão de funcionalidades nos botões de repeat e shuffle. Atualização do tempo e da barra de progresso da música.

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ const cover = document.getElementById('cover');
 const play = document.getElementById('play');
 const next = document.getElementById('next');
 const previous = document.getElementById('previous');
+const currentProgress = document.getElementById('current-progress');
+const progressContainer = document.getElementById('progress-container');
+const shuffleButton = document.getElementById('shuffle');
+const repeatButton = document.getElementById('repeat');
+const songTime = document.getElementById('song-time');
+const totalTime = document.getElementById('total-time');
 
 const Rising = {
     songName: 'Rising',
@@ -25,7 +31,10 @@ const Brutal = {
     filetwo: 'Brutal'
 };
 let isPlaying = false;
-const playlist = [Rising, AP, Brutal];
+let isShuffled = false;
+let repeatOn = false;
+const originalPlaylist = [Rising, AP, Brutal];
+let sortedPlaylist = [...originalPlaylist];
 let index = 0;
 
 function playSong(){
@@ -52,15 +61,15 @@ function playPauseDecider(){
 }
 
 function initializeSong(){
-    cover.src = `images/${playlist[index].filetwo}.jpg`;
-    song.src = `songs/${playlist[index].file}.mp3`;
-    songName.innerText = playlist[index].songName;
-    bandName.innerText = playlist[index].artist;
+    cover.src = `images/${sortedPlaylist[index].filetwo}.jpg`;
+    song.src = `songs/${sortedPlaylist[index].file}.mp3`;
+    songName.innerText = sortedPlaylist[index].songName;
+    bandName.innerText = sortedPlaylist[index].artist;
 }
 
 function previousSong(){
     if (index === 0) {
-         index = playlist.length - 1;
+         index = sortedPlaylist.length - 1;
     }
     else {
         index -= 1;
@@ -69,7 +78,7 @@ function previousSong(){
     playSong();
 }
 function nextSong(){
-    if (index === playlist.length - 1) {
+    if (index === sortedPlaylist.length - 1) {
          index = 0;
     }
     else {
@@ -79,8 +88,86 @@ function nextSong(){
     playSong();
 }
 
+function updateProgress(){
+    song.currentTime
+    song.duration
+    const barWidth = (song.currentTime/song.duration) * 100;
+    currentProgress.style.setProperty('--progress', `${barWidth}%`);
+    songTime.innerText = toHHMMSS(song.currentTime);
+}
+
+function jumpTo(event){
+    const width = progressContainer.clientWidth;
+    const clickPosition = event.offsetX;
+    const jumpToTime = (clickPosition/width)*song.duration;
+    song.currentTime = jumpToTime;
+}
+
+function shuffleArray(preShuffleArray){
+    const size = preShuffleArray.length;
+    let currentIndex = size - 1;
+    while(currentIndex > 0){
+        let randomIndex = Math.floor(Math.random()*size);
+        let aux = preShuffleArray[currentIndex];
+        preShuffleArray[currentIndex] = preShuffleArray[randomIndex];
+        preShuffleArray[randomIndex] = aux;
+        currentIndex -= 1;
+    }
+}
+
+function shuffleButtonClicked(){
+    if(isShuffled === false){
+        isShuffled = true;
+        shuffleArray(sortedPlaylist);
+        shuffleButton.classList.add('button-active');
+    }
+    else{
+        isShuffled = false;
+        sortedPlaylist = [...originalPlaylist];
+        shuffleButton.classList.remove('button-active'); 
+    }
+}
+
+function repeatButtonClicked(){
+    if(repeatOn === false) {
+        repeatOn = true;
+        repeatButton.classList.add('button-active');
+    }
+    else{
+        repeatOn = false;
+        repeatButton.classList.remove('button-active');
+    }
+}
+
+function nextOrRepeat() {
+    if(repeatOn === false){
+        nextSong();
+    }
+    else{
+        playSong();
+    }
+}
+
+function toHHMMSS(originalNumber){
+    let hours = Math.floor(originalNumber/3600);
+    let min = Math.floor((originalNumber - hours * 3600)/60);
+    let secs = Math.floor(originalNumber - hours * 3600 - min * 60);
+
+    return `${hours.toString().padStart(2, '0')}:${min.toString().padStart(2, '0')}:${secs.toString().padStart(2, '0')}`;
+}
+
+function updateTotalTime(){
+    totalTime.innerText = toHHMMSS(song.duration);
+}
+
 initializeSong();
 
 play.addEventListener('click', playPauseDecider);
 previous.addEventListener('click', previousSong);
 next.addEventListener('click', nextSong);
+song.addEventListener('timeupdate', updateProgress);
+song.addEventListener('ended', nextOrRepeat);
+song.addEventListener('loadedmetadata', updateTotalTime);
+progressContainer.addEventListener('click', jumpTo);
+shuffleButton.addEventListener('click', shuffleButtonClicked);
+repeatButton.addEventListener('click', repeatButtonClicked);
